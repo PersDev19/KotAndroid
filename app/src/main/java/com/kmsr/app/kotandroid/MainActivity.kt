9cb0477f3263647c457428715452b1887eabc3ec
PersDev19 <61111048+PersDev19@users.noreply.github.com>
2020-02-22 21:58:50 +0900

    Firebase Study
    2020.02.22 Firebase 스터디 진행중

--- app/src/main/java/com/kmsr/app/kotandroid/MainActivity.kt
+++ app/src/main/java/com/kmsr/app/kotandroid/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.kmsr.app.kotandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        auth = FirebaseAuth.getInstance()
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        Toast.makeText(this,"currentUser:$currentUser",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed()
+        val alertDialog = AlertDialog.Builder(this)
+
+        alertDialog.setTitle(resources.getString(R.string.info))
+        alertDialog.setMessage(R.string.msgFinish)
+        alertDialog.setPositiveButton("예", DialogInterface.OnClickListener { dialog, which -> finish()  })
+        alertDialog.setNegativeButton("아니오", DialogInterface.OnClickListener { dialog, which ->  })
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
---
+++ app/src/main/java/com/kmsr/app/kotandroid/SignActivity.kt
@@ -0,0 +1,104 @@
+package com.kmsr.app.kotandroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+
+class SignActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var googleSignInClient:GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        auth = FirebaseAuth.getInstance()
+
+        val loginButton = findViewById<SignInButton>(R.id.login_button)
+        loginButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn(){
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+//            updateUI(null)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    Toast.makeText(this,"user:$user",Toast.LENGTH_SHORT).show()
+//                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                    Snackbar.make(main_layout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+//                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    companion object{
+        private const val TAG = "SignActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+}
